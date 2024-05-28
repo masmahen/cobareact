5bec3e1a23bd246f94bbdb517ffa5072ffa20a91
Darezma <mahendradaffareza@gmail.com>
2024-05-28 23:10:39 +0700

    update app & language

--- belajar_react2/src/components/App.jsx
+++ belajar_react2/src/components/App.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
-import Language from './Language';
+import React from "react";
+import Language from "./Language";
 
 class App extends React.Component {
   render() {
+    // Tempelkan code yang disediakan di jendela instruksi ke bawah baris ini
+    const languageList = [
+      {
+        name: "HTML & CSS",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg",
+      },
+      {
+        name: "React",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg",
+      },
+      {
+        name: "Ruby",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg",
+      },
+      {
+        name: "Ruby on Rails",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg",
+      },
+    ];
+
     return (
       <div>
-        <h1>List of Languages</h1>
-        <div className='language'>
-          {/* Tambahkan props untuk HTML & CSS */}
-          <Language 
-            name= 'HTML & CSS'
-            image= 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
-          />
-          {/* Tambahkan props untuk JavaScript */}
-          <Language 
-            name= 'JavaScript'
-            image= 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
-            
-          />
-          {/* Tambahkan props untuk React */}
-          <Language 
-            name= 'React'
-            image= 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
-          />
+        <h1>List of Language</h1>
+        <div className="language">
+          {languageList.map((languageItem) => {
+            return (
+              <Language name={languageItem.name} image={languageItem.image} />
+            );
+          })}
         </div>
       </div>
     );
--- belajar_react2/src/components/Language.jsx
+++ belajar_react2/src/components/Language.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 class Language extends React.Component {
   render() {
     return (
-      <div className='language-item'>
-        <div className='language-name'>HTML & CSS</div>
-        <img className='language-image' src='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg' />
+      <div className="language-item">
+        <div className="language-name">{this.props.name}</div>
+        <img className="language-image" src={this.props.image} />
       </div>
     );
   }
